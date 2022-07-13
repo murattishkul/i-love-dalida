b837740b5e130c488bc182d23a693214db75889f
Murat Tishkul <murat.tishkul@gmail.com>
2022-07-13 06:37:01 +0600

    i love dalida)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import Button from '@mui/material/Button';
 import styled from '@emotion/styled';
 import heart from './heart.svg'
 import {useEffect, useState, useRef} from 'react';
+import Typography from '@mui/material/Typography';
+import { Configuration, OpenAIApi } from 'openai';
+import Loader from './Loader';
+import './some.scss';
+
 import img1 from './photos/1.jpg'
 import img2 from './photos/2.jpg'
 import img3 from './photos/3.jpg'
@@ -17,10 +22,23 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
 }
 
+function getRandomIntButNot(min, max, id){
+  let x = getRandomInt(min, max);
+  if(x == id) return getRandomIntButNot(min, max, id);
+  return x;
+}
+
 function App() {
+  const configuration = new Configuration({
+    apiKey: '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',
+  });
+  const openai = new OpenAIApi(configuration);
+
   const [topImage, setTopImage] = useState(img1);
   const [bottomImage, setBottomImage] = useState(img2);
   const [isEven, setIsEven] = useState(0);
+  const [compliment, setCompliment] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const topRef = useRef(null);
   const bottomRef = useRef(null);
@@ -31,7 +49,7 @@ function App() {
     backgroundSize: "cover",
   })
   useEffect(()=>{
-    document.addEventListener('mousemove', function(e) {
+    function handleMove(e) {
       let body = document.querySelector('body');
       let circle = document.createElement('span');
       let x = e.offsetX;
@@ -45,7 +63,11 @@ function App() {
       setTimeout(function() {
         circle.remove();
       }, 500);
-    });
+    }
+    document.addEventListener('mousemove', handleMove);
+    return () => {
+      document.removeEventListener('mousemove', handleMove)
+    }
   }, [])
 
   useEffect(() => {
@@ -59,16 +81,67 @@ function App() {
     })
   }, [])
 
-  const handleClick = () => {
+  const handleClick = async() => {
     let id = getRandomInt(0,7);
-    let id2 = getRandomInt(0,7);
+    let id2 = getRandomIntButNot(0,7, id);
 
     let photos = [img1, img2,img3, img4,img5, img6,img7]
     
+    console.log(topRef.current.classList)
+    // setBottomImage(photos[id]);
+    // setTopImage(photos[id2]);
+      
+    // if(isEven){
+    //   bottomRef.current.classList
+    //   .remove('topLoL')
+    //   bottomRef.current.classList
+    //     .add('bottomLol')
+
+    //   topRef.current.classList
+    //     .remove('bottomLol')
+    //   topRef.current.classList
+    //     .add('topLoL')
+    // }
+    // else {
+    //   bottomRef.current.classList
+    //   .remove('bottomLol')
+
+    //   bottomRef.current.classList
+    //     .add('topLoL')
+
+    //   topRef.current.classList
+    //     .remove('topLoL')
+    //   topRef.current.classList
+    //     .add('bottomLol')
+    // }
+
+    // setIsEven(prevState => !prevState)
+    setIsLoading(true);
+    const response = await openai.createCompletion({
+      model: "text-davinci-002",
+      prompt: "Write a funny, cute, seducing, teasing compliment to a woman called Dalida, who works in tech and from Kazakhstan and lives in Almaty and loves tennis and loves DJing and loves tall guys and charismatic and empathetic and environmentalist and ecologist and clever and well-educated and loves to read and loves to watch comedy and loves to travel and beautiful and who loves dance and who likes playing on dombra.\n",
+      temperature: 1,
+      max_tokens: 75,
+      top_p: 1,
+      frequency_penalty: 1.54,
+      presence_penalty: 1.68,
+    });
+
+    console.log(response)
+    console.log(response.data, response.data.choices[0], response.data.choices[0].text);
+
+    setCompliment(response.data.choices[0].text);
+
+  }
+
+  useEffect(() => {
+
+    let id = getRandomInt(0,7);
+    let id2 = getRandomIntButNot(0,7, id);
+
+    let photos = [img1, img2,img3, img4,img5, img6,img7]
     setBottomImage(photos[id]);
     setTopImage(photos[id2]);
-
-    console.log(topRef.current.classList)
       
     if(isEven){
       bottomRef.current.classList
@@ -84,6 +157,7 @@ function App() {
     else {
       bottomRef.current.classList
       .remove('bottomLol')
+
       bottomRef.current.classList
         .add('topLoL')
 
@@ -94,8 +168,9 @@ function App() {
     }
 
     setIsEven(prevState => !prevState)
-    
-  }
+
+    setIsLoading(false)
+  }, [compliment])
 
   return (
     <div className="App  heart container" id="cf4">
@@ -104,13 +179,29 @@ function App() {
       <img ref={bottomRef} className={`bottomLol`} src={bottomImage} />
       
       <header className="App-header heart">
-        <DalidaButton className="heart"  onClick={handleClick}>
+        {compliment && <DalidaCompliment>
+          {compliment}
+        </DalidaCompliment>}
+        { isLoading ? <Loader />:
+        <div id="heart">
+        <DalidaButton  className="heart"  onClick={handleClick}>
           bro i love dalida
         </DalidaButton>
+        </div>
+        }
       </header>
     </div>
   );
 }
+
+const DalidaCompliment = styled(Typography)`
+  font-size: 40px;
+  font-family: 'Edu QLD Beginner', cursive;
+  text-align: center;
+  width: 60%;
+  font-weight: 900;
+  z-index: 2;
+`;
 
 const DalidaButton = styled(Button)`
   border-radius: 20px;
@@ -120,6 +211,16 @@ const DalidaButton = styled(Button)`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  font-family: 'Edu QLD Beginner', cursive;
+  z-index: 5;
+  background: transparent;
+  font-weight: 900;
+  font-size: 20px;
+  max-width: 130px;
+  text-align: center;
+  min-height: 100px;
+}
 `;
+
 
 export default App;
---
+++ src/Loader.js
@@ -0,0 +1,16 @@
+
+const Loader = () => (
+    <div dangerouslySetInnerHTML={{__html: `
+    
+   <div class="loader">
+   
+   <svg version="1.1"  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="-466.4 259.6 280.2 47.3" enable-background="new -466.4 259.6 280.2 47.3" xml:space="preserve">
+       <polyline class="animation"  points="-465.4,281 -436,281 -418.9,281 -423.9,281 -363.2,281 -355.2,269 -345.2,303 -335.2,263 -325.2,291 -319.2,281 -187.2,281 "></polyline>
+       
+   </svg>
+</div>
+    `}}></div>
+
+)
+
+export default Loader;
